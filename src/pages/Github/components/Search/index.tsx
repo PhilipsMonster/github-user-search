47267d9966beda31d0e99b79a5276b09df36c00f
Marcelo Gomes <marcelosuptec@hotmail.com>
2021-03-11 07:16:14 -0300

    Finish user-search form

--- src/pages/Github/components/Search/index.tsx
+++ src/pages/Github/components/Search/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Button from '../../../../core/components/Button';
 import './styles.scss';
 
 const Search = () => {
     return (
-        <h1>Tela de pesquisa</h1>
+        <div className="search-container">
+            <div className="search-content card-base">
+                <h1 className="text-title">
+                    Encontre um perfil Github
+                </h1>
+                <input
+                    type="text"
+                    className="user-search"
+                    placeholder="Usuário Github"
+                />
+                <Button text="Encontrar" />
+            </div>
+        </div>
     );
 }
 
